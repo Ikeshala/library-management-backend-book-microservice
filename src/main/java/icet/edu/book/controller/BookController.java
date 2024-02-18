c0fd6976af2238f01a23df2bb26688371f6b1407
Ikeshala <ikeshala.peiris@gmail.com>
2024-02-19 00:06:54 +0530

    Created the yml file for configuration.

--- src/main/java/icet/edu/book/controller/BookController.java
+++ src/main/java/icet/edu/book/controller/BookController.java
@@ -2,15 +2,18 @@ package icet.edu.book.controller;
 
 import icet.edu.book.dto.Book;
 import icet.edu.book.service.BookService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
+    @Autowired
     BookService service;
-    @PostMapping("/add")
-    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
     public void addBook(@RequestBody Book book){
         service.addBook(book);
     }
--- src/main/java/icet/edu/book/service/impl/BookServiceImpl.java
+++ src/main/java/icet/edu/book/service/impl/BookServiceImpl.java
@@ -2,10 +2,11 @@ package icet.edu.book.service.impl;
 
 import icet.edu.book.dto.Book;
 import icet.edu.book.service.BookService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService {
     @Override
     public void addBook(Book book) {
-
     }
 }
