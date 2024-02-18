c60d5ae94129431c0fd2eaadcb531a72559a76af
Ikeshala <ikeshala.peiris@gmail.com>
2024-02-18 22:41:49 +0530

    Configured to be a Spring Boot application using the @SpringBootApplication annotation.

--- src/main/java/icet/edu/book/Main.java
+++ src/main/java/icet/edu/book/Main.java
@@ -1,6 +1,11 @@
 package icet.edu.book;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        SpringApplication.run(Main.class);
     }
 }
